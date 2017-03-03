aa5a33977fea2f2e7e437314401336dc98e932a6
umeshshirole <shiroleumesh@gmail.com>
2017-03-03 12:34:44 +0530

    Splash Screen Added Main Activity modified

--- MyApplication/app/src/main/java/com/example/semat/callbackhandingexample/SplashScreenActivity.java
+++ MyApplication/app/src/main/java/com/example/semat/callbackhandingexample/SplashScreenActivity.java
@@ -2,15 +2,12 @@ package com.example.semat.callbackhandingexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toast.makeText(this, "Hello " , Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_splash_screen);
     }
 }
